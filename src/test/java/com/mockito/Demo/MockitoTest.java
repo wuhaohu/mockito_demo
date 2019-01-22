100c94297fbc3f9d2bf89d0c4e6f683feaa8b7eb
wb-wh511153 <wb-wh511153@alibaba-inc.com>
2019-01-22 11:39:18 +0800

    maven init

---
+++ src/test/java/com/mockito/Demo/MockitoTest.java
@@ -0,0 +1,135 @@
+package com.mockito.Demo; /**
+ * @author: create by nadao
+ * @version: v1.0
+ * @description: PACKAGE_NAME
+ * @date:2019/1/21
+ */
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class MockitoTest {
+    @Test
+    public void verify_behaviour(){
+        //模拟创建一个List对象
+        List mock = mock(List.class);
+        //使用mock的对象
+        mock.add(1);
+        mock.clear();
+        //验证add(1)和clear()行为是否发生
+        verify(mock).add(1);
+        verify(mock).clear();
+    }
+
+    @Test
+    public void when_thenReturn(){
+        //mock一个Iterator类
+        Iterator iterator = mock(Iterator.class);
+        //预设当iterator调用next()时第一次返回hello，第n次都返回world
+        when(iterator.next()).thenReturn("hello").thenReturn("world");
+        //使用mock的对象
+        String result = iterator.next() + " " + iterator.next() + " " + iterator.next();
+        //验证结果
+        assertEquals("hello world world",result);
+    }
+
+    @Test(expected = IOException.class)
+    public void when_thenThrow() throws IOException {
+        OutputStream outputStream = mock(OutputStream.class);
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+        //预设当流关闭时抛出异常
+        doThrow(new IOException()).when(outputStream).close();
+        outputStream.close();
+    }
+
+    @Test
+    public void with_arguments(){
+        Comparable comparable = mock(Comparable.class);
+        //预设根据不同的参数返回不同的结果
+        when(comparable.compareTo("Test")).thenReturn(1);
+        when(comparable.compareTo("Omg")).thenReturn(2);
+        assertEquals(1, comparable.compareTo("Test"));
+        assertEquals(2, comparable.compareTo("Omg"));
+        //对于没有预设的情况会返回默认值
+        assertEquals(0, comparable.compareTo("Not stub"));
+    }
+
+    @Test
+    public void real_partial_mock(){
+        //通过spy来调用真实的api
+        List list = spy(new ArrayList());
+        assertEquals(0,list.size());
+        A a = mock(A.class);
+        //通过thenCallRealMethod来调用真实的api
+        when(a.doSomething(anyInt())).thenCallRealMethod();
+        assertEquals(999,a.doSomething(999));
+    }
+
+
+    class A{
+        public int doSomething(int i){
+            return i;
+        }
+    }
+
+    @Test
+    public void capturing_args(){
+        PersonDao personDao = mock(PersonDao.class);
+        PersonService personService = new PersonService(personDao);
+
+        ArgumentCaptor<Person> argument = ArgumentCaptor.forClass(Person.class);
+        personService.update(1,"jack");
+        verify(personDao).update(argument.capture());
+        assertEquals(1,argument.getValue().getId());
+        assertEquals("jack",argument.getValue().getName());
+    }
+
+    class Person{
+        private int id;
+        private String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    interface PersonDao{
+        public void update(Person person);
+    }
+
+    class PersonService{
+        private PersonDao personDao;
+
+        PersonService(PersonDao personDao) {
+            this.personDao = personDao;
+        }
+
+        public void update(int id,String name){
+            personDao.update(new Person(id,name));
+        }
+    }
+
+
+
+}
